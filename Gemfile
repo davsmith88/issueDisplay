c1d0afcbfe7b5d558f8f910d9177042d43095abf
Dave <davsmith88@gmail.com>
2015-01-02 18:57:41 +1000

    added more seed data

--- Gemfile
+++ Gemfile
@@ -51,6 +51,7 @@ group :development do
   gem 'brakeman', :require => false
   gem "better_errors"
   gem "binding_of_caller"
+  gem "awesome_print"
   # gem 'jazz_hands'
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# # This file should contain all the record creation needed to seed the database with its default values.
+# # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+# #
+# # Examples:
+# #
+# #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+# #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Seed data for web app useful for getting up and running very quickly
+
 
 User.destroy_all
 Role.destroy_all
 Right.destroy_all
 Grant.destroy_all
 Assignment.destroy_all
+Department.destroy_all
+Area.destroy_all
+DepartmentArea.destroy_all
 
-user = User.create!(:email => "new_user@testing.com", :password => "helloworld", :password_confirmation => "helloworld")
+department1 = Department.create!(name: "wet end", description: "random words")
+area1 = Area.create!(name: "modul facer", description: "more random words")
+DepartmentArea.create!(department_id: department1.id, area_id: area1.id, name: "wet end modal facer")
+
+
+Impact.create!(name: "Downtime", description: "issues that cause downtime, ie anytime the machine could stop")
+Impact.create!(name: "Waste", description: "Waste is the result of this issue")
+
+user = User.create!(name: "mya", title: "random", email: "viewer@testing.com", password: "helloworld", password_confirmation: "helloworld")
 user.roles << viewers = Role.create!(:name => "Viewer")
-
-author = User.create!(:email => "new_user_chad@testing.com", :password => "helloworld", :password_confirmation => "helloworld")
+author = User.create!(name: "second", title: "another", :email => "new_user_chad@testing.com", :password => "helloworld", :password_confirmation => "helloworld")
 author.roles << authors = Role.create!(:name => "Author")
-
-admin = User.create!(email: "admin@testing.com", password: "helloworld", password_confirmation: "helloworld")
+admin = User.create!(name: "thrid", title: "anther title", email: "admin@testing.com", password: "helloworld", password_confirmation: "helloworld")
 admin.roles << admins = Role.create!(name: "Admin")
 
 create = Right.create!(:resource => "issues", :operation => "CREATE")
